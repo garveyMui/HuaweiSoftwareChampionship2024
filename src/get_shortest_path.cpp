cf8723f792fa95adbaf42352dbd455bc96665a3f
garveyMui <m77561m@icloud.com>
2024-03-19 15:09:15 +0800

    change hyper parameter: max_value in get_shortest_path.cpp

--- src/get_shortest_path.cpp
+++ src/get_shortest_path.cpp
@@ -129,7 +129,7 @@ pair<int, queue<Position>> ShortestPathGetter::shortestPath(Position start, Posi
     // 20: 193612
     // 30: 192068
     // 40: 191278
-    int max_value = 88; // 寻找货物时记录
+    int max_value = 44; // 寻找货物时记录
     Position max_value_posi;
     int cur_level = 0;
     Position mark = q.front();
